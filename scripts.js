b26c3bca974f184552d62a790bc356fd26ca9c2b
Camilo Uribe <67170129+UribeCamilo98@users.noreply.github.com>
2025-10-22 17:05:27 -0500

    Mejoras en el Header
    Se ajustaron los media queries para el header en tablet y se realizaron cambios iniciales al hero.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const btn= document.querySelector("#menu-btn");
 const nav= document.querySelector("#primary-navigation");
 const header=document.querySelector("header");
+const links= document.querySelectorAll('#primary-navigation a');
 
 btn.addEventListener("click",function(){
 	if(btn.getAttribute("aria-expanded")=="false"){
@@ -12,4 +13,12 @@ btn.addEventListener("click",function(){
 	}
 });
 
+links.forEach((link)=>{
+	link.addEventListener("click", () => {
+		btn.setAttribute("aria-expanded","false");
+		nav.setAttribute("hidden","");
+	});
+});
+
 document.documentElement.style.setProperty("--header-height", header.offsetHeight + "px");
+document.documentElement.style.setProperty("--menu-count", links.length);
